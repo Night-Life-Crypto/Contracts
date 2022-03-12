92e701e97aa100fe5248cdde55dd9de60eeddf0f
Michal Sivak <sivakmichal@gmail.com>
2022-03-12 15:59:28 +1100

    add new version

--- contracts/NightLifeStakingV2.sol
+++ contracts/NightLifeStakingV2.sol
@@ -7,13 +7,14 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./PolarityTestToken.sol";
 
 contract UserRewards is Context, Ownable{
    
-    address public lpToken;
-    address public rewardToken;
-    uint256 public TOTALUSERREWARDS = 0;
-	address public NLIFEWallet;
+    address public _lpToken;
+    address public _rewardToken;
+    address public NLIFEWallet;
+    PolarityTestToken polarityToken;
 
     struct USERREWARD{
         uint256 userReward;     
@@ -32,25 +33,25 @@ contract UserRewards is Context, Ownable{
     mapping(address => USERREWARD) public _userRewardsDetails;
     address[] public _stakers;
 
-    constructor(address _rewardToken) public {
-        rewardToken = _rewardToken;
-        NLIFEWallet = msg.sender;
+    constructor(address rewardToken) public {
+        _rewardToken = rewardToken;
+        NLIFEWallet = _msgSender();
     }   
 
-    function updateNLIFEWallet(address _newAddr) public onlyOwner {
-        require(_newAddr != address(0), "Avoid Zero Address");
-        NLIFEWallet = _newAddr;
-    }
-
-   function getUserReward() public view returns (uint256) {
-       require(_userRewardsDetails[msg.sender].userReward > 0, "User is not staker.");
-       USERREWARD memory findUser = _userRewardsDetails[msg.sender];
+  
+   function getUserReward(address _staker) public view returns (uint256) {
+       require(_userRewardsDetails[_staker].userReward < 0, "User is not staker.");
+       USERREWARD memory findUser = _userRewardsDetails[_staker];
        return findUser.userReward;
    }
 
 
    function totalRewards() public view returns(uint256){
-       return getCurrentNLIFEBalance() - totalUserRewards();
+       return totalWalletBalance() - totalUserRewards();
+   }
+
+   function totalWalletBalance() public view returns(uint256){
+     return IERC20(_rewardToken).balanceOf(address(this));
    }
 
     function totalUserRewards() public view returns (uint256){
@@ -72,93 +73,56 @@ contract UserRewards is Context, Ownable{
     }
 
    // Calculate Claim Reward when user claims
-    function claimUserRewardsV2(uint256 amount, address staker) public{      
-        USERREWARD memory userRew = _userRewardsDetails[staker];
+    function claimUserRewardsV2(address staker) public view returns(uint256) {             
         STAKE memory stakerUser = _stakeInfo[staker];
-        uint partitionStakeAmount = SafeMath.div(stakerUser.amount, totalStakeAmount());
-        uint totalRewards = SafeMath.sub(getCurrentNLIFEBalance(),userRew.userReward); 
-        uint userIncrementReward = SafeMath.mul(totalRewards, partitionStakeAmount);
-        uint newUserIncrementReward = SafeMath.mul(amount,partitionStakeAmount);
-        _userRewardsDetails[staker].userReward = SafeMath.add(newUserIncrementReward, userRew.userReward);   
+        USERREWARD memory userRew = _userRewardsDetails[staker];
+        uint256 partitionStakeAmount = SafeMath.div(stakerUser.amount * 100, totalStakeAmount());
+        uint256 totalRewardsAmount = SafeMath.sub(totalWalletBalance(),totalUserRewards()); 
+        uint256 userIncrementReward = SafeMath.div(SafeMath.mul(totalRewardsAmount, partitionStakeAmount), 100);       
+        return userIncrementReward;
     }
 
-    // Total Wallet Balance
-    function getCurrentNLIFEBalance() public view returns (uint256){
-        // return IERC20(NLIFEWallet).balanceOf(address(this));
-        // only for testing 
-        uint256 balance = 10000;
-        balance;
+    // set reward to
+    function setRewardToken(address rewardToken) public onlyOwner {
+        require(_rewardToken != address(0), "Avoid Zero Address");
+        _rewardToken = rewardToken;
     }
 
-
-    function isStakeHolder(address _account) public view returns (bool) {
-        return _stakeInfo[_account].amount > 0;
+    function setLPToken(address lpToken) public onlyOwner {
+        require(_rewardToken != address(0), "Avoid Zero Address");
+        _lpToken = lpToken;
     }
 
-    function setLPToken(address _lpToken) public onlyOwner {
-        lpToken = _lpToken;
-    }
-
-    function setRewardToken(address _rewardToken) public onlyOwner {
-        rewardToken = _rewardToken;
-    }
-
-    function rewardTokenAddr() public view returns (address) {
-        return rewardToken;
-    }
-
-    function dummySetStaker(address staker, uint256 amount) public onlyOwner{
-       _stakers.push(staker);
-       _stakeInfo[staker].amount = amount;
-    }
-
-    function rewardOf(address _staker) public view returns (uint256) {
-        STAKE memory _stakeDetail = _stakeInfo[_staker];
-
-        uint256 _rewards = totalRewards();
-        uint256 _singlePart =
-            SafeMath.mul(_stakeDetail.amount, SafeMath.sub(block.timestamp, _stakeDetail.lastUpdatedAt));
-
-        uint256 _totalPart;
-
-        for (uint256 i = 0; i < _stakers.length - 1; i++) {
-            STAKE memory _singleStake = _stakeInfo[_stakers[i]];
-            _totalPart =  SafeMath.add(_totalPart, (SafeMath.mul(_singleStake.amount, SafeMath.sub(block.timestamp, _singleStake.lastUpdatedAt))));
-        }
-
-        if (_totalPart == 0) return 0;
-
-        return SafeMath.div(SafeMath.mul(_rewards, _singlePart),_totalPart);
-    }
+    function _rewardTokenAddr() public view returns (address) {
+        return _rewardToken;
+    }   
 
     function stake(uint256 _amount) public {
-        IERC20(lpToken).transfer(address(this), _amount);
-        STAKE storage _stake = _stakeInfo[_msgSender()];
-
+        // send fund to stake holding wallet        
+        IERC20(_lpToken).transferFrom(_msgSender(), address(this), _amount);
+        STAKE storage _stake = _stakeInfo[_msgSender()];  
         if (_stake.amount > 0) {
-            uint256 reward = rewardOf(_msgSender());
+            uint256 reward = claimUserRewardsV2(msg.sender);
             _stake.lastUpdatedAt = block.timestamp;
             _stake.amount = SafeMath.add(_stake.amount, _amount);
-            IERC20(rewardToken).transfer(_msgSender(), reward);
-            emit Withdraw(_msgSender(), reward);
+            claimReward();
+            emit Stake(_msgSender(), _amount);
         } else {
             _stake.lastUpdatedAt = block.timestamp;
             _stake.amount = _amount;
             _stakers.push(_msgSender());
-        }
-        emit Stake(_msgSender(), _amount);
+            emit Stake(_msgSender(), _amount);
+        }       
     }
 
     function unstake() public {
         require(_stakeInfo[_msgSender()].amount > 0, "Not staking");
-
         STAKE storage _stake = _stakeInfo[_msgSender()];
-        uint256 reward = rewardOf(_msgSender());
         uint256 amount = _stake.amount;
         _stake.amount = 0;
         _stake.lastUpdatedAt = block.timestamp;
-
-        IERC20(rewardToken).transfer(_msgSender(), reward);
+        // claim for user reward and re-calculate for rest of them
+        claimReward();
         for (uint256 i = 0; i < _stakers.length; i++) {
             if (_stakers[i] == _msgSender()) {
                 _stakers[i] = _stakers[_stakers.length - 1];
@@ -169,19 +133,27 @@ contract UserRewards is Context, Ownable{
 
         uint256 fee = SafeMath.div(amount ,100);
         uint256 _amount = SafeMath.sub(amount, fee);
-        IERC20(lpToken).transfer(_msgSender(), _amount);
-        IERC20(lpToken).transfer(NLIFEWallet, fee);
+        IERC20(_lpToken).transfer(_msgSender(), _amount);
+        IERC20(_lpToken).transfer(NLIFEWallet,fee);
         emit Unstake(_msgSender(), amount);
     }
 
     function claimReward() public {
         STAKE storage _stake = _stakeInfo[_msgSender()];
-        uint256 reward = rewardOf(_msgSender());
+        // Calculate for user who claim reward           
         _stake.lastUpdatedAt = block.timestamp;
-        for(uint i = 0; i < _stakers.length - 1; i++){
-           claimUserRewardsV2(0, _stakers[i]);
-        }
-        IERC20(rewardToken).transfer(_msgSender(), reward);
-        emit Withdraw(_msgSender(), reward);
+        uint256 reward = claimUserRewardsV2(_msgSender());      
+
+        // iterate through rest of the users       
+        for(uint i = 0; i < _stakers.length; i++){  
+           if(_stakers[i] != _msgSender()){                     
+            uint256 newReward = claimUserRewardsV2(_stakers[i]);
+            _userRewardsDetails[_stakers[i]].userReward = SafeMath.add(newReward, _userRewardsDetails[_stakers[i]].userReward);
+           }            
+        }        
+        uint256 totalReward = SafeMath.add(reward, _userRewardsDetails[_msgSender()].userReward);
+        _userRewardsDetails[_msgSender()].userReward = 0; //SafeMath.add(reward, _userRewardsDetails[_msgSender()].userReward);
+        IERC20(_rewardToken).transfer(_msgSender(), totalReward);
+        emit Withdraw(_msgSender(), totalReward);
     }
   }
